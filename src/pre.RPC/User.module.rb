0bbe2a3e14218fc79529082daae7fd2aea06176a
Anatoly Chernov <chertoly@gmail.com>
2021-05-18 03:17:57 +0300

    Explain why RPC.find_by_token is as it is

--- src/pre.RPC/User.module.rb
+++ src/pre.RPC/User.module.rb
@@ -2,13 +2,29 @@ def self.roles
   @roles ||= []
 end
 
+# Accepts a `token`(String).
+#
+# This method is for finding a user.
+# It is expected by Hobby::Auth:
+# https://github.com/ch1c0t/hobby-auth
+#
+# If you store all active sessions in one place,
+# you might want to redefine it,
+# since the default implementation below is O(n).
+#
+# Still, it seems to be a reasonable default,
+# because it's ready, by default, for cases when
+# there might be multiple kinds of tokens and multiple ways to store them.
+# And, thanks to Ruby, it's easy to redefine it when it's not needed.
+#
+# Returns a user or nil(if a session for such token does not exist).
 def self.find_by_token token
   roles.each do |role|
     if user = (role.find_by_token token)
       return user
     end
   end
-  return false
+  return nil
 end
 
 def can? _function
